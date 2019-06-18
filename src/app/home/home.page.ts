2df458012f7c877cbfff6372cb99097318b8fc2b
榊原昌彦 <sakakibara@rdlabo.jp>
2019-06-18 14:03:08 +0900

    非同期にDOMを追加

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,13 @@ export class HomePage {
 
   constructor() {}
 
+  ionViewWillEnter() {
+    const preloadArea: HTMLElement = document.getElementById('preload');
+    preloadArea.appendChild(document.createElement('ion-card'));
+    preloadArea.appendChild(document.createElement('ion-card-header'));
+    preloadArea.appendChild(document.createElement('ion-card-subtitle'));
+    preloadArea.appendChild(document.createElement('ion-card-title'));
+    preloadArea.appendChild(document.createElement('ion-card-content'));
+    preloadArea.appendChild(document.createElement('ion-back-button'));
+  }
 }
