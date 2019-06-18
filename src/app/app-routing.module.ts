46661c1e01c8d1f06eb0ae6b6c836f7cded6f916
榊原昌彦 <sakakibara@rdlabo.jp>
2019-06-18 10:37:43 +0900

    add(page): create next page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'next', loadChildren: './next/next.module#NextPageModule' },
 ];
 
 @NgModule({
